06dd014f1ecf167ebea9968bdcb76317827f4e80
ToshaBY <anton.rybakou@gmail.com>
2022-04-04 14:18:03 +0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Body} from './site/Body';
+import {Footer} from './site/Footer';
+import {Header} from "./site/Header";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <>
+            <Header title={'NEW BODY'}/>
+            <Body/>
+            <Footer/>
+        </>
+    );
 }
 
 export default App;
