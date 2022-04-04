021e9c1de54606df064f18b68a3e3174bf72a507
ToshaBY <anton.rybakou@gmail.com>
2022-04-04 16:37:28 +0300

    feat: video - 2 (map), add css

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import './App.css';
-import {Body} from './site/Body';
-import {Footer} from './site/Footer';
-import {Header} from "./site/Header";
 import {NewComponent} from "./NewComponent";
+
+const topCars = [
+    {manufacturer: 'BMW', model: 'm5cs'},
+    {manufacturer: 'Mercedes', model: 'e63s'},
+    {manufacturer: 'Audi', model: 'rs6'},
+];
 
 export const App = () => {
     return (
         <>
-            <NewComponent students={'new component'}/>
-            {/*<Header title={'NEW HEADER'}/>*/}
-            {/*<Body titleForBody={'NEW BODY'}/>*/}
-            {/*<Footer titleForFooter={'NEW FOOTER'}/>*/}
+            <NewComponent data={topCars}/>
         </>
     );
 }
--- src/NewComponent.tsx
+++ src/NewComponent.tsx
@@ -1,27 +1,37 @@
 import React from "react";
+import n from './NewComponent.module.css';
+
 
 type NewComponentType = {
-    // students: Array<StudentType>,
-    students: StudentType[],
+    data: Array<TopCarsType>,
 }
-type StudentType = {
-    id: number,
-    name: string,
-    age: number,
+
+type TopCarsType = {
+    manufacturer: string;
+    model: string;
 }
 
 
 export const NewComponent = (props: NewComponentType) => {
     return (
-        <ul>
-            {props.students.map((objectFromStudentArray, index) => {
-                return (
-                    <li key={objectFromStudentArray.id}>
-                        <span>Name: {objectFromStudentArray.name}</span>
-                        <span>Age: {objectFromStudentArray.age}</span>
-                    </li>
-                )
-            })}
-        </ul>
+        <>
+            <table className={n.table}>
+                <tr className={n.table__titles}>
+                    <th className={n.table__title}>Index</th>
+                    <th className={n.table__title}>Manufacturer</th>
+                    <th className={n.table__title}>Model</th>
+                </tr>
+                {props.data.map((data, index) => {
+                        return (
+                            <tr className={n.table__items}>
+                                <td className={n.table_item} key={index}>{index + 1}</td>
+                                <td className={n.table_item} key={index}>{data.manufacturer}</td>
+                                <td className={n.table_item} key={index}>{data.model}</td>
+                            </tr>
+                        )
+                    }
+                )}
+            </table>
+        </>
     )
 }
