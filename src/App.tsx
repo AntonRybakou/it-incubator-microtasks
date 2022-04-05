9e0c4f8436eb6b1e4a2a22d455e31f780cf8ac59
ToshaBY <anton.rybakou@gmail.com>
2022-04-05 12:36:51 +0300

    feat: video 3 (button)

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,35 @@
 import React from 'react';
 import './App.css';
-import {NewComponent} from "./NewComponent";
+import {Button} from "./components/Button";
+// import {NewComponent} from "./components/NewComponent";
 
-const topCars = [
-    {manufacturer: 'BMW', model: 'm5cs'},
-    {manufacturer: 'Mercedes', model: 'e63s'},
-    {manufacturer: 'Audi', model: 'rs6'},
-];
+// const topCars = [
+//     {manufacturer: 'BMW', model: 'm5cs'},
+//     {manufacturer: 'Mercedes', model: 'e63s'},
+//     {manufacturer: 'Audi', model: 'rs6'},
+// ];
 
 export const App = () => {
+
+    const Button1Foo = (subscriber: string, age: number) => {
+        console.log(`Hello, I'm ${subscriber} and I'm ${age} years old`)
+    }
+
+    const Button2Foo = (subscriber: string, age: number) => {
+        console.log(`Hello, I'm ${subscriber} and I'm ${age} years old`);
+    }
+
+    const Button3Foo = () => {
+        console.log("I'm STUPID");
+    }
+
+
+
     return (
         <>
-            <NewComponent data={topCars}/>
+            <Button name={'YouTubeChannel-1'} callBack={() => Button1Foo("VASYA", 21)}/>
+            <Button name={'YouTubeChannel-2'} callBack={() => Button2Foo('IVAN', 30)}/>
+            <Button name={'Stupid Button'} callBack={Button3Foo}/>
         </>
     );
 }
